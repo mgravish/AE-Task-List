bf6592b7f17e16e5ae0d84febe8011ef13920260
Matt <mgravish@gmail.com>
2018-02-10 11:25:33 -0800

    Check boxes and styling
    Check boxes are now working and styling is in place. Next step is to figure out how to use cookies to store the lists.

--- js/tasks.js
+++ js/tasks.js
@@ -1,4 +1,4 @@
-var newItem = '<div class="task-line"><div class="check-box"><svg height="28" width="28"><g id="check-box" stroke="none" class="test" stroke-width="1" fill="none" fill-rule="evenodd"><circle cx="14" cy="14" r="10" fill="#979797" fill-opacity="0.1" stroke="#979797" stroke-width="3" onclick="clickHandler(event)"/><polyline id="Path-2" stroke="#FFFFFF" stroke-width="4" points="8 14 13 18 20 10" onclick="clickHandler(event)"/></g></svg></div><input type="text" class="task" placeholder="Write a task..." onkeydown="keyHandler(event)">';
+var newItem = '<div class="task-line"><div class="check-box"><svg height="28" width="28"><g id="check-box" stroke="none" class="test" stroke-width="1" fill="none" fill-rule="evenodd"><circle cx="14" cy="14" r="10" fill="#57a56c" fill-opacity="0.1" stroke="#848484" stroke-width="2" onclick="clickHandler(event)"/><polyline id="check" stroke="#FFFFFF" stroke-width="4" points="8 14 13 18 20 10" onclick="clickHandler(event)"/></g></svg></div><input type="text" class="task" placeholder="Write a task..." onkeydown="keyHandler(event)">';
 
 
 function keyHandler(e){
@@ -44,12 +44,19 @@ function keyHandler(e){
 
 function clickHandler(e){
     var circ = $(e.target.parentElement).find("circle")[0];
+    var check = $(e.target.parentElement).find("polyline")[0];
+    var text = $(e.target.parentElement.parentElement.parentElement.parentElement).find("input")[0];
     if($(circ).css("fill-opacity")==0.1) {
-        $(circ).css({ fill: "#FFD442" });
         $(circ).css({ fillOpacity: "1.0" });
+        $(check).css({ transform: "scale(1)" });
+        $(text).css({ color: "#828282" });
+        $(text).css({ textDecoration: "line-through" });
+        
     }
     else {
-        $(circ).css({ fill: "#FFD442" });
         $(circ).css({ fillOpacity: "0.1" });
+        $(check).css({ transform: "scale(0)" });
+        $(text).css({ color: "white" });
+        $(text).css({ textDecoration: "none" });
     }
 }
